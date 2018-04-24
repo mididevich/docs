bb00c1b8336c342d0bc6d882d34d944f345b414b
Mikhail Didevich <atmeelos@gmail.com>
2018-04-24 20:36:13 +0300

    Update telegrambot.php

--- telegrambot.php
+++ telegrambot.php
@@ -1,43 +1,40 @@
 <?php
     include('vendor/autoload.php'); //Подключаем библиотеку
     use Telegram\Bot\Api; 
-    $telegram = new Api('589088703:AAEOxhv4-VGIzRNgQFIKzkIb8We-Z5Yla8c'); //Устанавливаем токен, полученный у BotFather
+
+    $telegram = new Api('375466075:AAEARK0r2nXjB67JiB35JCXXhKEyT42Px8s'); //Устанавливаем токен, полученный у BotFather
     $result = $telegram -> getWebhookUpdates(); //Передаем в переменную $result полную информацию о сообщении пользователя
+    
     $text = $result["message"]["text"]; //Текст сообщения
     $chat_id = $result["message"]["chat"]["id"]; //Уникальный идентификатор пользователя
     $name = $result["message"]["from"]["username"]; //Юзернейм пользователя
-    $keyboard = [["Копать"],["Охота"],["Рыбалка"]]; //Клавиатура
-    $hunt = [["снарка"],["черепаху"],["летающую свинью"],["носорога"],["утенка"],["волка Гланка"],["медведя"],["онаниста"],["корову"],["енота"],["мишку коалу"],["лису"],["мышку"],["корову"],["енота"],["Дурова"]];
-    $hunt_place = [["тундре"],["саване"],["кустарнике"],["полумраке бара"],["зарослях конопли"],["перезоде метро"],["офисе роскомнадзора"]];
-    $top_hunt = [[0],['nickname']];
-    if ($text){
-        if ($text == "/start") {
+    $keyboard = [["Последние статьи"],["Картинка"],["Гифка"]]; //Клавиатура
+
+    if($text){
+         if ($text == "/start") {
             $reply = "Добро пожаловать в бота!";
             $reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false ]);
             $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup ]);
-        }
-        if($text == "!копать"){
-            $numer_place =  mt_rand(0, count($hunt_place) - 1);
-            $number_hunt =  mt_rand(0, count($hunt) - 1);
-            $weight = mt_rand(1, 1500 - 1);
-            $reply1 = 'Вы затаились в ' . $hunt_place[$number_place] . ' и поджидаете... Вы что-то слышите и стреляете в том направлении...';
-            $reply2 = 'Поздравляю, ' . $name . ' Вы только что положили в свой мешок ' . $hunt[$number_hunt] . '! Весом в' . $weight . 'кило!'
-            if ($weight > $top_hunt[1]){
-                $top_hunt[1] = $weight;
-                $top_hunt[2] = $name;
-                $reply3 = 'Поздравляю,' . $name . 'Вы установили новый рекорд по охоте!';
-            }
-            else {
-                $reply3 = 'Извини,' . $name . ', но это не новый рекорд! Однако, хорошая попытка';
-            }
+        }elseif ($text == "/help") {
+            $reply = "Информация с помощью.";
+            $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply ]);
+        }elseif ($text == "Картинка") {
+            $url = "https://68.media.tumblr.com/6d830b4f2c455f9cb6cd4ebe5011d2b8/tumblr_oj49kevkUz1v4bb1no1_500.jpg";
+            $telegram->sendPhoto([ 'chat_id' => $chat_id, 'photo' => $url, 'caption' => "Описание." ]);
+        }elseif ($text == "Гифка") {
+            $url = "https://68.media.tumblr.com/bd08f2aa85a6eb8b7a9f4b07c0807d71/tumblr_ofrc94sG1e1sjmm5ao1_400.gif";
+            $telegram->sendDocument([ 'chat_id' => $chat_id, 'document' => $url, 'caption' => "Описание." ]);
+        }elseif ($text == "Последние статьи") {
+            $html=simplexml_load_file('http://netology.ru/blog/rss.xml');
+            foreach ($html->channel->item as $item) {
+	     $reply .= "\xE2\x9E\xA1 ".$item->title." (<a href='".$item->link."'>читать</a>)\n";
+        	}
             $telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode' => 'HTML', 'disable_web_page_preview' => true, 'text' => $reply ]);
-            sleep(2);
-            $telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode' => 'HTML', 'disable_web_page_preview' => true, 'text' => $reply2 ]);
-            sleep(2);
-            $telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode' => 'HTML', 'disable_web_page_preview' => true, 'text' => $reply3 ]);
+        }else{
+        	$reply = "По запросу \"<b>".$text."</b>\" ничего не найдено.";
+        	$telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode'=> 'HTML', 'text' => $reply ]);
         }
-    }
-    else{
-        $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => "Отправьте текстовое сообщение." ]);
+    }else{
+    	$telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => "Отправьте текстовое сообщение." ]);
     }
 ?>
